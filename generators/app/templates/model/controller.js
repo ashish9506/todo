b655ce42a776963c8e3bd7e7abdd1922f24752b3
Mitesh Sondhi <miteshsondhi@cropsly.com>
2021-07-12 16:09:03 +0530

    cleanup

--- generators/app/templates/model/controller.js
+++
@@ -1,6 +0,0 @@
-const Controller = require('../../lib/controller')
-const <%= model.camelName %>Facade = require('./facade')
-
-class <%= model.pascalName %>Controller extends Controller {}
-
-module.exports = new <%= model.pascalName %>Controller(<%= model.camelName %>Facade)
--- generators/app/templates/model/schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const <%= model.camelName %>Schema = new Schema({
-  title: { type: String, required: true },
-  body: { type: String }
-})
-
-module.exports = <%= model.camelName %>Schema
